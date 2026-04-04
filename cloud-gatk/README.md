97f3bf029754d6cdc251dde84cc85f0f8c4acf92
Debbie227 <110264572+Debbie227@users.noreply.github.com>
2026-04-04 23:47:31 +0000

    began cloud workflow

---
+++ cloud-gatk/README.md
@@ -0,0 +1,20 @@
+# Bash commands for cloud pipeline
+
+- Created variant-pipeline.sh with commands up to variant calling
+- Created dockerfile to run pipeline in
+- created job.json to submit to the cloud
+- created environment.yaml for conda downloads
+
+```bash
+# Download data using fasterq-dump and upload to cloud bucket for use in pipeling
+conda install bioconda::sra-tools
+fasterq-dump -p SRR12023503 --split-files
+# Connection failed
+# Used pixi from the shell commands pipeline to download
+```
+### Next steps
+
+- Permissions to use google cloud with gcloud auth and gcloud projects
+- Lock the environment using conda lock
+- Build and push to Google Container Registry using gcloud build
+- Submit the job gcloud batch jobs
---
+++ cloud-gatk/variant-pipeline.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -euo pipefail
+
+# Variables
+SAMPLE=SRR12023503
+BUCKET=gatk-resource-bucket
+
+WORKDIR=/mnt/disks/work # Directory for storing large working files in Google Cloud Batch
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+echo "Copying input data..."
+gsutil cp gs://$BUCKET/data/${SAMPLE}_*.fastq.gz .
+
+echo "Copying reference..."
+gsutil cp gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta .
+gsutil cp gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.fai .
+gsutil cp gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.dict .
+
+echo "Indexing reference..."
+bwa index Homo_sapiens_assembly38.fasta
+
+echo "Running FastQC..."
+fastqc ${SAMPLE}_*.fastq.gz
+
+echo "Trimming reads..."
+trim_galore \
+  --paired \
+  --quality 20 \
+  --length 50 \
+  ${SAMPLE}_1.fastq.gz ${SAMPLE}_2.fastq.gz
+
+echo "Running alignment..."
+bwa mem -t 8 \
+  -R "@RG\tID:$SAMPLE\tSM:$SAMPLE\tPL:ILLUMINA" \
+  Homo_sapiens_assembly38.fasta \
+  ${SAMPLE}_1_val_1.fq.gz ${SAMPLE}_2_val_2.fq.gz \
+  > ${SAMPLE}.sam
+
+echo "Sorting BAM..."
+samtools sort ${SAMPLE}.sam -o ${SAMPLE}.bam
+samtools index ${SAMPLE}.bam
+
+echo "Marking duplicates..."
+gatk MarkDuplicates \
+  -I ${SAMPLE}.bam \
+  -O ${SAMPLE}_marked.bam \
+  -M metrics.txt \
+  --CREATE_INDEX true
+
+echo "BQSR..."
+gatk BaseRecalibrator \
+  -I ${SAMPLE}_marked.bam \
+  -R Homo_sapiens_assembly38.fasta \
+  --known-sites gs://gcp-public-data--broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
+  --known-sites gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf.gz \
+  -O recal.table
+
+gatk ApplyBQSR \
+  -I ${SAMPLE}_marked.bam \
+  -R Homo_sapiens_assembly38.fasta \
+  --bqsr-recal-file recal.table \
+  -O ${SAMPLE}_recal.bam
+
+echo "Variant calling..."
+gatk HaplotypeCaller \
+  -R Homo_sapiens_assembly38.fasta \
+  -I ${SAMPLE}_recal.bam \
+  -O ${SAMPLE}.vcf.gz
+
+echo "Uploading results..."
+gsutil cp *.fastq.gz *.bam *.bai .vcf.gz gs://$BUCKET/results/
+
+echo "DONE"
